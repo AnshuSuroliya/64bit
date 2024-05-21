74da8cedb599c20f303710fe63c2cfaef14053f5
2000108447 <rohit1@hexaware.com>
2024-05-21 18:19:38 +0530

    updated Home and Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,92 +4,177 @@ import { Link, useNavigate } from "react-router-dom";
 import { getUserData, logout } from "../reducers/authReducer";
 
 const Navbar = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const [menuOpen, setMenuOpen] = useState(false);
-    
-    useEffect(() => {
-        dispatch(getUserData({ email: localStorage.getItem("email") }));
-    }, [dispatch]);
-    
-    const handleLogout = () => {
-        dispatch(logout());
-        navigate("/");
-    }
-    
-    const userData = useSelector((state) => state.auth.userData);
-    
-    return (
-        <div className="text-white">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex justify-between items-center h-24">
-                    <div className="flex-shrink-0">
-                        <h2 className="text-3xl font-bold">InterviewAI</h2>
-                    </div>
-                    <div className="hidden md:flex space-x-8">
-                        <Link className="text-lg font-bold" to="/">Home</Link>
-                        <Link className="text-lg font-bold" to="/about">About Us</Link>
-                        {localStorage.getItem("jwt") ? (
-                            <div className="relative">
-                                <button className="peer flex items-center space-x-2 text-lg font-bold">
-                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                         fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                                    </svg>
-                                    <p>{userData.firstName}</p>
-                                </button>
-                                <div className="hidden peer-hover:flex hover:flex flex-col absolute bg-white text-black drop-shadow-xl border-t-2 border-[#31cfe2] right-0 mt-2">
-                                    <button className="px-5 py-3 hover:bg-gray-100 hover:font-bold" onClick={handleLogout}>Logout</button>
-                                </div>
-                            </div>
-                        ) : (
-                            <Link className="text-lg font-bold flex items-center" to="/login">
-                                <svg xmlns="http://www.w3.org/2000/svg"
-                                     fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-1">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                                </svg>
-                                Sign In
-                            </Link>
-                        )}
-                    </div>
-                    <div className="md:hidden">
-                        <button type="button" className="text-white" onClick={() => setMenuOpen(!menuOpen)}>
-                            <svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 6.75h15M4.5 12h15m-15 5.25h15" />
-                            </svg>
-                        </button>
-                    </div>
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    dispatch(getUserData({ email: localStorage.getItem("email") }));
+  }, [dispatch]);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
+
+  const userData = useSelector((state) => state.auth.userData);
+
+  return (
+    <div className="text-white z-[1000]">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-24">
+          <div className="flex-shrink-0">
+            <h2 className="text-3xl font-bold">InterviewAI</h2>
+          </div>
+          <div className="hidden md:flex space-x-8">
+            <Link className="text-lg font-bold" to="/">
+              Home
+            </Link>
+            <Link className="text-lg font-bold" to="/about">
+              About Us
+            </Link>
+            {localStorage.getItem("jwt") ? (
+              <div className="relative">
+                <button className="peer flex items-center space-x-2 text-lg font-bold">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-8 h-8"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                    />
+                  </svg>
+                  <p>{userData.firstName}</p>
+                </button>
+                <div className="hidden peer-hover:flex hover:flex flex-col absolute bg-white text-black drop-shadow-xl border-t-2 border-[#31cfe2] right-0 mt-2">
+                  <button
+                    className="px-5 py-3 hover:bg-gray-100 hover:font-bold"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </button>
                 </div>
-            </div>
-            {menuOpen && (
-                <div className="md:hidden bg-[#19173d] text-white px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                    <Link className="text-lg font-bold block" to="/" onClick={() => setMenuOpen(false)}>Home</Link>
-                    <Link className="text-lg font-bold block" to="/about" onClick={() => setMenuOpen(false)}>About Us</Link>
-                    {localStorage.getItem("jwt") ? (
-                        <div className="relative">
-                            <button className="peer flex items-center space-x-2 text-lg font-bold">
-                                <svg xmlns="http://www.w3.org/2000/svg"
-                                     fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                                </svg>
-                                <p>{userData.firstName}</p>
-                            </button>
-                            <div className="hidden peer-hover:flex hover:flex flex-col absolute bg-white text-black drop-shadow-xl border-t-2 border-[#31cfe2] right-0 mt-2">
-                                <button className="px-5 py-3 hover:bg-gray-100 hover:font-bold" onClick={handleLogout}>Logout</button>
-                            </div>
-                        </div>
-                    ) : (
-                        <Link className="text-lg font-bold flex justify-center" to="/login" onClick={() => setMenuOpen(false)}>
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                                 fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-1">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                            </svg>
-                            Sign In
-                        </Link>
-                    )}
-                </div>
+              </div>
+            ) : (
+              <Link className="text-lg font-bold flex items-center" to="/login">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6 mr-1"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                  />
+                </svg>
+                Sign In
+              </Link>
             )}
+          </div>
+          <div className="md:hidden">
+            <button
+              type="button"
+              className="text-white"
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              <svg
+                className="h-8 w-8"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 6.75h15M4.5 12h15m-15 5.25h15"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-    )
-}
+      </div>
+      {menuOpen && (
+        <div className="md:hidden bg-[#19173d] text-white px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <Link
+            className="text-lg font-bold block"
+            to="/"
+            onClick={() => setMenuOpen(false)}
+          >
+            Home
+          </Link>
+          <Link
+            className="text-lg font-bold block"
+            to="/about"
+            onClick={() => setMenuOpen(false)}
+          >
+            About Us
+          </Link>
+          {localStorage.getItem("jwt") ? (
+            <div className="relative">
+              <button className="peer flex items-center space-x-2 text-lg font-bold">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-8 h-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                  />
+                </svg>
+                <p>{userData.firstName}</p>
+              </button>
+              <div className="hidden peer-hover:flex hover:flex flex-col absolute bg-white text-black drop-shadow-xl border-t-2 border-[#31cfe2] right-0 mt-2">
+                <button
+                  className="px-5 py-3 hover:bg-gray-100 hover:font-bold"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+              </div>
+            </div>
+          ) : (
+            <Link
+              className="text-lg font-bold flex justify-center"
+              to="/login"
+              onClick={() => setMenuOpen(false)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6 mr-1"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                />
+              </svg>
+              Sign In
+            </Link>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
 export default Navbar;
--- src/homeScreen/Content.jsx
+++ src/homeScreen/Content.jsx
@@ -1,23 +1,58 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import photo from '../assets/hand.png';
+import photo from "../assets/hand.png";
 
 const Content = () => {
-    return (
-        <div className="relative w-full max-[874px]:flex-col max-[874px]:relative p-8 flex   mt-24 items-center md:items-start">
-            <div className="z-10 basis-2/4  w-full min-[874px]:w-6/12 lg:w-4/12 ml-0  text-center min-[874px]:text-left">
-                <div className=" text-[41px]/[45px] font-bold text-center text-white">Schedule your mock interview with InterviewAI today</div>
-                <div className="flex mt-6 justify-evenly ">
-                    <Link className="px-8 py-2 rounded-full text-white text-lg bg-gradient-to-r from-[#28528d] to-[#31cfe2] hover:from-[#023a89] to-[#31cfe2]" to="/schedule">
-                        Start Mock
-                    </Link>
-                    <button className="ml-4 md:ml-12 px-6 py-2 rounded-full text-white text-lg border-white border-2">
-                        Join Interview
-                    </button>
-                </div>
-            </div>
+  let txtArray = [];
+  let ind = 0;
+  var tmout = null;
+  useEffect(() => {
+    txtArray = document.getElementById("textchanger").querySelectorAll("h1");
+    if (tmout != null) {
+      clearTimeout(tmout);
+    }
+    setTimeout(animate, 500);
+  }, []);
 
+  async function animate() {
+    let prev_ind = ind - 1;
+    if (ind == 0) {
+      prev_ind = txtArray.length - 1;
+    }
+
+    txtArray[prev_ind]?.classList.add("-translate-x-[400px]");
+    txtArray[ind]?.classList.remove("-translate-x-[400px]");
+
+    ind = (ind + 1) % txtArray.length;
+    tmout = setTimeout(animate, 2000);
+  }
+
+  return (
+    <div className="w-screen h-screen z-[100]">
+      <div className="flex flex-col mt-[100px] md:mt-[200px] text-start px-3 md:px-6">
+        <h1 className="text-2xl md:text-5xl text-white font-semibold mb-4 z-[100]">
+          Everythin you need to
+        </h1>
+        <div id="textchanger" className="flex h-[50px] relative transition-all">
+          <h1 className="text-2xl md:text-3xl font-semibold text-blue-300 absolute top-0 left-0 -translate-x-[400px] transition-all duration-200">
+            Be Interview Ready
+          </h1>
+          <h1 className="text-2xl md:text-3xl font-semibold text-blue-300 absolute top-0 left-0 -translate-x-[400px] transition-all duration-200">
+            LevelUp Yourself
+          </h1>
+          <h1 className="text-2xl md:text-3xl font-semibold text-blue-300 absolute top-0 left-0 -translate-x-[400px] transition-all duration-200">
+            Access Yourself
+          </h1>
         </div>
-    );
+        <Link
+          className="px-3 py-2 text-white border-blue-200 border-2 w-[150px] rounded-lg z-[100]"
+          to="/schedule"
+        >
+          Schedule Now
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Content;

4f5b0199247ddfd7370a65d8c37017c96a1e137e
2000108447 <rohit1@hexaware.com>
2024-05-22 16:09:55 +0530

    App js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
        <Route path='' element={<Home/>}/>
        <Route path='/login' element={<Login/>}/>
        <Route path='/signup' element={<Register/>}/>
-       <Route path='/schedule' element={<Schedule/>}/>
+       <Route path='/schedule/:mock' element={<Schedule/>}/>
        <Route path='/interview' element={<NewInterview/>}/>
        
        <Route path='/test' element={<Interview/>}/>
